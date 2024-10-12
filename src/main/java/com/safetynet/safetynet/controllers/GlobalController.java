a5da1c8f95b2f94cfe5b361eed76db1b329255a8
airwin17 <faitourdja@gmail.com>
2024-10-12 03:38:17 +0200

    adding openapi documentation about api

--- src/main/java/com/safetynet/safetynet/controllers/GlobalController.java
+++ src/main/java/com/safetynet/safetynet/controllers/GlobalController.java
@@ -20,34 +20,49 @@ import com.safetynet.safetynet.dto.FireDTO;
 import com.safetynet.safetynet.dto.PersonDTO;
 import com.safetynet.safetynet.dto.PersonFirestrationDTO;
 import com.safetynet.safetynet.services.GlobalService;
+
+import io.swagger.v3.oas.annotations.Operation;
 @RestController
 public class GlobalController {
     @Autowired
     private GlobalService globalService;
     Logger logger = LoggerFactory.getLogger(GlobalController.class);
+    @Operation(summary = "This url should return a list of people covered by the fire station\r\n" + //
+                "corresponding. So, if the station number = 1, it should return the inhabitants\r\n" + //
+                "covered by the station number 1. The list should include the specific information\r\n" + //
+                "below: first name, last name, address, phone number. It should also provide a count\r\n" + //
+                "of the number of adults and the number of children (any individual aged 18 years or\r\n" + //
+                "less) in the area served.\r")
     @GetMapping(value = "/firestation", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<PersonFirestrationDTO> getFirestation(@RequestParam("stationNumber") String stationNumber) throws IOException, JsonProcessingException{
         logger.info("getFirestation", stationNumber);
         return ResponseEntity.ok(globalService.getFirestationByFirestationNumber(stationNumber));
     }
+    @Operation(summary = "This url should return a list of children (any individual aged 18 years or\r\n" + //
+                "less) living at this address. The list should include the first name and last name of\r\n" + //
+                "each child, their age and a list of other members of the household. If there are no\r\n" + //
+                "children, this url can return an empty string.")
     @GetMapping(value ="/childAlert", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<List<ChildAlertDTO>> getChildAlert(@RequestParam("address") String address) throws IOException{
         logger.info("getChildAlert", address);
         return ResponseEntity.ok(globalService.getChildAlert(address));
     }
+    @Operation(summary="This url should return a list of phone numbers of residents served by the fire station.")
     @GetMapping(value ="/phoneAlert", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<String>> gethPhoneAlert(@RequestParam("fireStationNumber") String fireStationNumber) throws IOException{
         logger.info("gethPhoneAlert", fireStationNumber);
         return ResponseEntity.ok(globalService.gethPhoneAlert(fireStationNumber));
     }
+    @Operation(summary = "This url should return the list of inhabitants living at the given address, as well as the fire station serving the address.")
     @GetMapping(value ="/fire", produces = MediaType.APPLICATION_JSON_VALUE) 
     @ResponseBody
     public ResponseEntity<FireDTO> getFire(@RequestParam("adress") String adress) throws IOException{
         logger.info("getFire", adress);
         return ResponseEntity.ok(globalService.getFire(adress));
     }
+    @Operation(summary = "This url should return a list of all households served by the fire station. This list should group the people by address. It should also include the name, phone number and age of the inhabitants, and list the medical records (medications, dosage and allergies) next to each name.\r")
     @GetMapping(value ="/flood/stations", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<List<AdressDTO>> getFloodStations(@RequestParam("stationNumber") String[] stationNumber) throws IOException{
@@ -57,12 +72,14 @@ public class GlobalController {
         logger.info("getFloodStations", str);
         return ResponseEntity.ok(globalService.getFloodStations(stationNumber));
     }
+    @Operation(summary="This url should return the name, address, age, email and medical records (medications, dosage and allergies) of each inhabitant. If several people have the same name, they should all appear.\r")
     @GetMapping(value ="/personInfolastName", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<List<PersonDTO>> getPersonInfolastName(@RequestParam("lastName") String lastName) throws IOException{
         logger.info("getPersonInfolastName", lastName);
         return ResponseEntity.ok(globalService.getPersonInfolastName(lastName));
     }
+    @Operation(summary = "This url should return the email addresses of all inhabitants of the city.")
     @GetMapping(value ="/communityEmail", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity<List<String>> getCommunityEmail(@RequestParam String city) throws IOException{
